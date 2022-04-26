c1db0606e881af628176d2c55530b613ba918c5e
EC2 Default User <ec2-user@ip-172-31-29-17.ec2.internal>
2022-04-26 09:18:38 +0000

    [update]サンプルを基に修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'book_comments/create'
-  get 'book_comments/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "homes#top"
